b0c7d817ed505f05bc2dab836a04824243938f34
Sean Starnes <seanstar12@gmail.com>
2014-06-11 13:56:00 -0500

    Updated to 1.3. Added Progress Bar and support for all boards

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 function enable() {
-  var aids = document.createElement('div');
-  aids.style.height = '400px';
-  aids.style.fontSize = '80px';
-  aids.id = 'bfresh';
-  document.body.appendChild(aids);
+  
+  var progressBar = document.createElement('div');
+  progressBar.style.width = '100%';
+  progressBar.style.backgroundColor = '#A20F0F';
+  progressBar.id = 'bfreshBar';
+
+  var progressBarProgress = document.createElement('div');
+  progressBarProgress.style.height = '85px';
+  progressBarProgress.style.width = '0%';
+  progressBarProgress.style.backgroundColor = '#28DD8E';
+  progressBarProgress.id = 'bfreshProgress';
+
+  var progressBarText = document.createElement('div');
+  progressBarText.style.height = '85px';
+  progressBarText.style.fontSize = '70px';
+  progressBarText.style.color = '#fff';
+  progressBarText.id = 'bfresh';
+ 
+  var buffer = document.createElement('div');
+  buffer.style.height = '275px';
+  buffer.id = 'bFreshBuffer';
+ 
+  document.body.appendChild(progressBar);
+  document.body.appendChild(buffer);
+  document.getElementById('bfreshBar').appendChild(progressBarProgress);
+  document.getElementById('bfreshProgress').appendChild(progressBarText);
 
   setInterval( function() {
     var totalHeight, currentScroll, visibleHeight;
@@ -19,12 +40,16 @@ function enable() {
     
     var temp = ((currentScroll+visibleHeight) / totalHeight)*100;
     if (temp < 100) {
+      if (temp >= 95){
+        var scale =  (temp - 95) * 20 ;
+        document.getElementById('bfreshProgress').style.width = scale + "%";    
+      }
       document.getElementById('bfresh').innerHTML = temp + "%";    
     } else {
       document.getElementById('bfresh').innerHTML = "Refreshing Page...";    
     }
     if (totalHeight <= currentScroll + visibleHeight ) {
-      window.location = "http://boards.4chan.org/b/";
+      window.location = window.location.href;
     }
   }, 100);
 };
