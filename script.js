1e53522373502a0d59884ec5a2efda466e58b299
Sean Starnes <seanstar12@gmail.com>
2014-06-11 10:36:40 -0500

    Added percentage to bottom. Made it huge.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function enable() {
   var aids = document.createElement('div');
-  aids.style.height = '350px';
+  aids.style.height = '400px';
+  aids.style.fontSize = '80px';
+  aids.id = 'bfresh';
   aids.innerHTML = "Keep Scrolling For Justice";
   document.body.appendChild(aids);
 
@@ -16,6 +18,12 @@ function enable() {
     totalHeight = document.body.offsetHeight;
     visibleHeight = document.documentElement.clientHeight;
     
+    var temp = ((currentScroll+visibleHeight) / totalHeight)*100;
+    if (temp < 100) {
+      document.getElementById('bfresh').innerHTML = temp + "%";    
+    } else {
+      document.getElementById('bfresh').innerHTML = "Refreshing Page...";    
+    }
     if (totalHeight <= currentScroll + visibleHeight ) {
       window.location = "http://boards.4chan.org/b/";
     }
